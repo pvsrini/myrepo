c8121cff00d3904f7f898ba11954c9f08778808a
PV Srinivas < pvsrini@yahoo.com>
2014-06-28 16:04:17 -0700

    prime numbers

---
+++ first/prime.js
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+
+// Returns true if n is prime, false otherwise
+var isPrime = function(n)
+{
+    var i = 2;
+    if (n <= 1) return false;
+    for (i = 2; i < n; i++) {
+      if (n % i == 0) return false;
+    }
+    return true;
+};
+
+// Find first K Prime numbers
+var firstkPrime = function(k) {
+    var arr = [];
+    var count = 0;
+    var i = 2;
+    while (count < k) {
+      if (isPrime(i)) {
+        arr.push(i);
+        ++count;
+      }
+      ++i;
+    }
+    return arr;
+};
+
+// Print to console
+var fmt = function(arr) {
+    return arr.join(",");
+};
+
+var k = 117;
+console.log("firstkPrime(" + k + ")");
+console.log(fmt(firstkPrime(k)));
